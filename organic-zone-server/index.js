11e47b3d49460c3e4dee8c5ca683c8120ea547f8
Israt Munia <isratmunia000@gmail.com>
2022-05-15 01:51:51 +0600

    load data in database

--- organic-zone-server/index.js
+++ organic-zone-server/index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
@@ -12,22 +13,58 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://munia:XA5tunhCXLD2Wmx7@cluster0.qcbxd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("organic-zone").collection("inventories");
-  console.log('connected to db')
-  // perform actions on the collection object
-//   client.close();
-});
+
+
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('organicZone').collection('inventory');
+
+        app.get('/inventory', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const inventories = await cursor.toArray();
+            res.send(inventories);
+        });
+
+        app.get('/inventory/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query={_id: ObjectId(id)};
+            const inventory = await serviceCollection.findOne(query);
+            res.send(inventory);
+        });
+
+        // POST
+        // app.post('/inventory', async(req, res) =>{
+        //     const newService = req.body;
+        //     const result = await serviceCollection.insertOne(newService);
+        //     res.send(result);
+        // });
+
+        // DELETE
+        // app.delete('/inventory/:id', async(req, res) =>{
+        //     const id = req.params.id;
+        //     const query = {_id: ObjectId(id)};
+        //     const result = await serviceCollection.deleteOne(query);
+        //     res.send(result);
+        // });
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 
 
 app.get('/', (req, res) => {
-    res.send('Server running');
+    res.send('Server running Organic Zone');
 });
 
 app.listen(port, () => {
-    console.log('Listening to port', port);
+    console.log('Port Listening', port);
 });
