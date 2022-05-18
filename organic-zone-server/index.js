15bc2e4bf71a3805a915ecd1047eec7de2317ea2
Israt Munia <isratmunia000@gmail.com>
2022-05-18 23:57:17 +0600

    again modified

--- organic-zone-server/index.js
+++ organic-zone-server/index.js
@@ -13,8 +13,6 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qcbxd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-console.log(process.env.DB_USER);
-console.log(process.env.DB_PASSWORD);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
